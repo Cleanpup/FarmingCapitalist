067e16eaf5542a70ff67d79d9344488a83312ded
CleanPup <cleanpup@pop-os.(none)>
2026-02-27 19:23:54 -0600

    Commit workspace changes

--- ModConfig.cs
+++
@@ -1,9 +0,0 @@
-namespace FarmingCapitalist
-{
-    /// <summary>The mod configuration model read from the player's <c>config.json</c> file.</summary>
-    public class ModConfig
-    {
-        /// <summary>Whether to enable debug logging.</summary>
-        public bool EnableDebugLogging { get; set; } = false;
-    }
-}
